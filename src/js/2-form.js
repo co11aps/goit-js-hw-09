9937f21d91fe2d29ce5140f6ace87182e364689a
co11aps <143988142+co11aps@users.noreply.github.com>
2024-02-08 12:58:50 +0800

    11

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,57 @@
-console.log('Form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  const data = {
+    email,
+    message,
+  };
+
+  saveToLS(LOCAL_STORAGE_KEY, data);
+}
+
+function saveToLS(key, value) {
+  const formContent = JSON.stringify(value);
+  localStorage.setItem(key, formContent);
+}
+
+function loadFromLS(key) {
+  const formContent = localStorage.getItem(key);
+  try {
+    return JSON.parse(formContent);
+  } catch {
+    return formContent;
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  const data = {
+    email,
+    message,
+  };
+
+  console.log(data);
+
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  form.reset();
+}
+
+function init() {
+  const data = loadFromLS(LOCAL_STORAGE_KEY) || {};
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+}
+
+init();
